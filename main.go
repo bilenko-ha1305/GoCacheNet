4fc7afcda8a87c4ba3848235bfcbb59ed2f0d61d
Hryhoriy <dinozzzzzawrik@gmail.com>
2023-07-21 21:57:35 +0300

    init

---
+++ main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Redis struct {
+	data map[string]interface{}
+	mu   sync.Mutex
+}
+
+type RedisCommand struct {
+	Command string      `json:"command"`
+	Key     string      `json:"key"`
+	Value   interface{} `json:"value"`
+}
+
+func NewRedis() *Redis {
+	return &Redis{
+		data: make(map[string]interface{}),
+	}
+}
+
+func (r *Redis) Set(key string, value interface{}) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	r.data[key] = value
+}
+
+func (r *Redis) Get(key string) (interface{}, bool) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	value, exists := r.data[key]
+	return value, exists
+}
+
+func (r *Redis) Delete(key string) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+	delete(r.data, key)
+}
+
+func (r *Redis) Expire(key string, duration time.Duration) {
+	go func() {
+		<-time.After(duration)
+		r.Delete(key)
+	}()
+}
+
+func (r *Redis) HandleCommand(command string, key string, value interface{}) interface{} {
+	switch command {
+	case "set":
+		r.Set(key, value)
+		return "OK"
+	case "get":
+		if val, exists := r.Get(key); exists {
+			return val
+		} else {
+			return "Key not found"
+		}
+	case "del":
+		r.Delete(key)
+		return "OK"
+	default:
+		return "Unknown command"
+	}
+}
+
+func handleConnection(conn net.Conn, redis *Redis) {
+	defer conn.Close()
+
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		input := scanner.Text()
+		if input == "exit" {
+			break
+		}
+
+		parts := strings.Split(input, " ")
+		if len(parts) < 2 {
+			conn.Write([]byte("Invalid command format.\n"))
+			continue
+		}
+
+		command := parts[0]
+		key := parts[1]
+		value := interface{}(nil)
+		if len(parts) > 2 {
+			value = strings.Join(parts[2:], " ")
+		}
+
+		result := redis.HandleCommand(command, key, value)
+		response, _ := json.Marshal(result)
+		conn.Write(append(response, '\n'))
+	}
+}
+
+func main() {
+	redis := NewRedis()
+
+	// Запускаем TCP-сервер на порту 8080
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server started on :8080")
+
+	// Основной цикл сервера для принятия подключений
+	go func() {
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Error accepting connection:", err)
+				continue
+			}
+
+			fmt.Println("New connection from:", conn.RemoteAddr())
+			go handleConnection(conn, redis)
+		}
+	}()
+
+	// Основной цикл для чтения команд из консоли
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := strings.ToLower(scanner.Text())
+
+		if input == "exit" {
+			break
+		}
+
+		parts := strings.Split(input, " ")
+		if len(parts) < 2 {
+			fmt.Println("Invalid command format.")
+			continue
+		}
+
+		command := parts[0]
+		key := parts[1]
+		value := interface{}(nil)
+		if len(parts) > 2 {
+			value = strings.Join(parts[2:], " ")
+		}
+
+		result := redis.HandleCommand(command, key, value)
+		fmt.Println(result)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
+	}
+}
